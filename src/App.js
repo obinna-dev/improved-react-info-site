13c5da408b06fd0d6116d893c545ff15b5e35c94
iamthefastone <o.osisiogu@gmail.com>
2022-05-02 13:12:16 +0100

    Clean up code

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import Main from "../src/components/Main"
 import "../src/styles.css"
 
 export default function App() {
-
     const [darkMode, setDarkMode] = React.useState("true")
-
     function toggleDarkMode()   {
     setDarkMode(prevDarkMode => !prevDarkMode)
 }
-
     return (
         <div className="container">
             <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
